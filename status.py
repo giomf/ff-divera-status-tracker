d275f41988f472b1037c20dd1fa6e0686b5f8ebb
Guillaume Fournier <>
2024-02-20 20:33:02 +0100

    Fix incorrect float representation

--- status.py
+++ status.py
@@ -8,10 +8,10 @@ def calculate_on_duty_percentage(input: Path, off_duty_keyword: str, output: Pat
         percentage = _calculate_on_duty_percentage(series, off_duty_keyword)
         result[name] = [percentage]
 
-    result.transpose().to_excel(output, header=None)
+    result.transpose().to_excel(output, header=None, float_format='%.1f')
 
 def _calculate_on_duty_percentage(data: pd.Series, off_duty_keyword: str) -> int:
     total = data.count()
     off_duty_count = data.str.count(off_duty_keyword).sum()
-    return '%.1f' % (100 - (off_duty_count / total * 100))
+    return 100 - (off_duty_count / total * 100)
 
