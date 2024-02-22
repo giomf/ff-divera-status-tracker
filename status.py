745d853a7d15cca751223e2fec8db4cf28d07834
Guillaume Fournier <>
2024-02-23 00:10:17 +0100

    Add weekend duty

--- status.py
+++ status.py
@@ -3,15 +3,27 @@ from pathlib import Path
 
 def calculate_on_duty_percentage(input: Path, off_duty_keyword: str, output: Path):
     result = pd.DataFrame()
-    data = pd.read_excel(input, index_col=0)
+    data = pd.read_excel(input, index_col=0, parse_dates=True)
+
     for name, series in data.items():
-        percentage = _calculate_on_duty_percentage(series, off_duty_keyword)
-        result[name] = [percentage]
+        total_percentage = _calculate_on_duty_total_percentage(series, off_duty_keyword)
+        weekend_percentage = _calculate_on_duty_weekend_percentage(series, off_duty_keyword)
+        result[name] = [total_percentage, weekend_percentage]
 
-    result.transpose().to_excel(output, header=None, float_format='%.1f')
+    result.transpose().to_excel(output, float_format='%.1f', index_label='Name', header=['Total on duty', 'Weekend on duty'])
 
-def _calculate_on_duty_percentage(data: pd.Series, off_duty_keyword: str) -> int:
-    total = data.count()
-    off_duty_count = data.str.count(off_duty_keyword).sum()
-    return 100 - (off_duty_count / total * 100)
+def _calculate_on_duty_total_percentage(data: pd.Series, off_duty_keyword: str) -> float:
+    total_count = data.count()
+    total_off_duty_count = data.str.count(off_duty_keyword).sum()
 
+    return _calculate_on_duty_percentage_from_off_duty(total_off_duty_count, total_count)
+
+def _calculate_on_duty_weekend_percentage(data: pd.Series, off_duty_keyword: str) -> float:
+    weekend = data[data.index.dayofweek > 4]
+    weekend_total_count = weekend.count()
+    weekend_off_duty_count = weekend.str.count(off_duty_keyword).sum()
+
+    return _calculate_on_duty_percentage_from_off_duty(weekend_off_duty_count, weekend_total_count)
+
+def _calculate_on_duty_percentage_from_off_duty(part: int, total: int) -> float:
+    return 100 - (part / total * 100)
