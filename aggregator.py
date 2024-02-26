069c1e7d3a334da925d73513e99a34397850cc06
Guillaume Fournier <>
2024-02-26 11:14:55 +0100

    Add off duty without note

--- aggregator.py
+++ aggregator.py
@@ -13,11 +13,14 @@ def get_exel_files(directory: Path) -> [Path]:
 
 def aggregate(directory: Path, output: Path):
     result = pd.DataFrame()
-    for idx, status_part in enumerate(get_exel_files(directory)):
+    for _, status_part in enumerate(get_exel_files(directory)):
         wb = openpyxl.load_workbook(status_part).active
         date = datetime.datetime.strptime(wb['B1'].value, _DATE_TIME_INPUT_FORMAT).strftime(_DATE_TIME_OUTPUT_FORMAT)
-        status_sheet = pd.read_excel(status_part, skiprows=3, header=None, usecols=[0, 1], names=['Name', date], index_col='Name')
-        result = pd.concat([result, status_sheet], axis=1)
+        status_sheet = pd.read_excel(status_part, skiprows=3, header=None, usecols=[0, 1, 3], names=['Name', 'Status', 'Note'], index_col='Name')
 
-    result = result.transpose()
-    result.to_excel(output)
+        index = pd.MultiIndex.from_product([status_sheet.index, [date]], names=['Name', 'Date'])
+        data = {'Status': status_sheet['Status'].values, 'Note': status_sheet['Note'].values}
+        status_at_date = pd.DataFrame(data, index=index)
+
+        result = pd.concat([result, status_at_date])
+    result.sort_index().to_excel(output)
--- status.py
+++ status.py
@@ -1,16 +1,22 @@
 import pandas as pd
 from pathlib import Path
 
+_HEADER=['Total on duty', 'Weekend on duty', 'Off duty without note']
+
 def calculate_on_duty_percentage(input: Path, off_duty_keyword: str, output: Path):
     result = pd.DataFrame()
-    data = pd.read_excel(input, index_col=0, parse_dates=True)
+    data = pd.read_excel(input, index_col=[0, 1], parse_dates=['Date'])
+    names = data.index.get_level_values(0).drop_duplicates()
 
-    for name, series in data.items():
-        total_percentage = _calculate_on_duty_total_percentage(series, off_duty_keyword)
-        weekend_percentage = _calculate_on_duty_weekend_percentage(series, off_duty_keyword)
-        result[name] = [total_percentage, weekend_percentage]
+    for name in names:
+        name_data = data.loc[name]
+        status = name_data['Status']
+        total_percentage = _calculate_on_duty_total_percentage(status, off_duty_keyword)
+        weekend_percentage = _calculate_on_duty_weekend_percentage(status, off_duty_keyword)
+        without_note_percentage = _calculate_off_duty_without_note_percentage(name_data, off_duty_keyword)
+        result[name] = [total_percentage, weekend_percentage, without_note_percentage]
 
-    result.transpose().to_excel(output, float_format='%.1f', index_label='Name', header=['Total on duty', 'Weekend on duty'])
+    result.transpose().to_excel(output, float_format='%.1f', index_label='Name', header=_HEADER)
 
 def _calculate_on_duty_total_percentage(data: pd.Series, off_duty_keyword: str) -> float:
     total_count = data.count()
@@ -25,5 +31,13 @@ def _calculate_on_duty_weekend_percentage(data: pd.Series, off_duty_keyword: str
 
     return _calculate_on_duty_percentage_from_off_duty(weekend_off_duty_count, weekend_total_count)
 
+def _calculate_off_duty_without_note_percentage(data: pd.DataFrame, off_duty_keyword: str) -> float:
+    total_off_duty_count = data['Status'].str.count(off_duty_keyword).sum()   
+    off_duty_without_note = len(data[(data['Status'] == off_duty_keyword) & (data['Note'].isnull())])    
+    return _calculate_percentage(off_duty_without_note, total_off_duty_count)
+
 def _calculate_on_duty_percentage_from_off_duty(part: int, total: int) -> float:
-    return 100 - (part / total * 100)
+    return 100 - _calculate_percentage(part, total)
+
+def _calculate_percentage(part: int, total: int) -> float:
+    return part / total * 100
